b980e6a7396b1497d530db59def808803f1ddb52
raisercostin <raisercostin@gmail.com>
2020-03-16 21:48:08 +0200

    refactor

--- src/main/java/org/raisercostin/utils/Locations.java
+++
@@ -1,7 +0,0 @@
-package org.raisercostin.utils;
-
-public class Locations {
-	public static ClassPathInputLocation classpath(String resource) {
-		return new ClassPathInputLocation(resource);
-	}
-}
